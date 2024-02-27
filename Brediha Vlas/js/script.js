9ab6b724ced62ebc4bf7d4b441b82f2fea6c4285
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2024-02-28 01:54:22 +0200

    start

---
+++ Brediha Vlas/js/script.js
@@ -0,0 +1,47 @@
+let currentStep = 1;
+
+function nextStep() {
+  document.getElementById("step" + currentStep).style.display = "none";
+  currentStep++;
+  document.getElementById("step" + currentStep).style.display = "block";
+}
+
+function prevStep() {
+  document.getElementById("step" + currentStep).style.display = "none";
+  currentStep--;
+  document.getElementById("step" + currentStep).style.display = "block";
+}
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+let isMuted = false;
+
+function toggleMute(muteButton) {
+  muteButton.classList.toggle("muted");
+  if (muteButton.classList.contains("muted")) {
+    muteButton.style.backgroundImage = 'url("/image/soundoff.png")';
+  } else {
+    muteButton.style.backgroundImage = 'url("/image/sound.png")';
+  }
+}
